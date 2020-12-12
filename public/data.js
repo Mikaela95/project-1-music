559bc997f8ea29512b40d2964c73e9005963e7d3
mikaela.verhoosel <mikaela.verhoosel@gmail.com>
2020-12-13 08:54:57 +1100

    removed unused code, added FontAwesome icons in index.html file, toggle fav button

--- public/data.js
+++ public/data.js
@@ -2,14 +2,6 @@
 let searchHistory = [];
 let favourited = [];
 
-// Icons
-let starIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
-<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-</svg>`;
-let starredIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16" style="color: yellow">
-<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.283.95l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
-</svg>`;
-
 // Query Spotify API and return response
 $("#search").on("click", function (e) {
   e.preventDefault();
@@ -27,9 +19,6 @@ $("#search").on("click", function (e) {
   });
 });
 
-/* $("#spotify-button").on("click", function() {
-  $(".test-star").append(starIcon);
-}); */
 
 // Add to list
 const addToList = (queryArtist, queryResponse) => {
@@ -37,11 +26,15 @@ const addToList = (queryArtist, queryResponse) => {
   // Push to array
   // searchArtist.push(queryArtist);
 
-  // Append to list in the DOM - include star, id and artists name
+  // Append to list in the DOM - include id and artists name
   $("#search-history").append(
-    `<li class="list-group-item">${queryArtist}<Button class="btn btn-primary" id="${idArtist}">${starIcon}</Button></li>`
+    `<li class="list-group-item">${queryArtist}<button class="btn btn-primary" id="${idArtist}"><span class="fa fa-star"></span></button></li>`
   );
+
   $(`#${idArtist}`).on("click", function () {
+    // change button and star colour classes
+    $(this).toggleClass("btn-warning")
+    // add a favourite
     addToFavourites();
   });
 };
